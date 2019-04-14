55800a9966c46fc355eadb520b00b562b389bcf1
diarra <bakarydiarra8509@gmail.com>
2019-04-14 23:05:16 +0200

    premier commit

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -10,7 +10,7 @@
 session_start();
 
 // Chemin du site :
-define('RACINE_SITE', '/Bar_berger/'); // on d�finit le chemin de la racine du site pour pouvoir �tablir des url de fichiers en chemin absolu que l'on soit dans un template admin ou front
+define('RACINE_SITE', '/'); // on d�finit le chemin de la racine du site pour pouvoir �tablir des url de fichiers en chemin absolu que l'on soit dans un template admin ou front
 
 // D�claration de variables d'affichage de contenus :
 $contenu = '';
