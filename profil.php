262d81a68a59f76f5dd697632786e6a5070d83a8
diarra <bakarydiarra8509@gmail.com>
2019-04-17 20:00:18 +0200

    mandrill

--- profil.php
+++ profil.php
@@ -1,5 +1,7 @@
 <?php
 require_once('inc/init.inc.php');
+require_once('mandrill/src/Mandrill.php');
+$mandrill = new Mandrill('565ZYIb4RHDrJ2vGG-kM9A');
 
 //--------------------- TRAITEMENT -------------------
 // Redirection si visiteur non connecté :
